e4e75d2fdb0a8981d2118c7479924f65213ae514
Matthew Alexander <matthewalexander353@gmail.com>
2023-02-27 13:40:59 -0500

    Added functionality to Alarms Page Navigation Bar. Removed Redundancies in AndroidManifest.xml

--- LookOut/app/src/main/java/com/example/lologin/AlarmActivity.kt
+++ LookOut/app/src/main/java/com/example/lologin/AlarmActivity.kt
@@ -1,7 +1,9 @@
 package com.example.lologin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.lologin.ui.login.LoginActivity
 import com.google.android.material.tabs.TabLayout
 
 
@@ -9,7 +11,25 @@ class AlarmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarms)
-        //Navigation bar value
+
+        //Navigation bar
         val navigationBar = findViewById<TabLayout>(R.id.navigation_bar)
+        navigationBar.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                when (tab.position) {
+                    0 -> startActivity(Intent(this@AlarmActivity, LoginActivity::class.java))
+                    1 -> startActivity(Intent(this@AlarmActivity, TimerActivity::class.java))
+                    // Add more cases for each tab as needed
+                }
+            }
+            //
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                // Handle tab unselection
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+                // Handle tab reselection
+            }
+        })
     }
 }
