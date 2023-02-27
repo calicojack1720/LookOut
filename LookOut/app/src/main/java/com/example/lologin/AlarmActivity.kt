ea59986cd84b8d938a707eb4b0b5ecec7c08b181
PluggedCloud542 <52136084+PluggedCloud542@users.noreply.github.com>
2023-02-26 20:57:08 -0500

    Added navigationBar value

--- LookOut/app/src/main/java/com/example/lologin/AlarmActivity.kt
+++ LookOut/app/src/main/java/com/example/lologin/AlarmActivity.kt
@@ -1,11 +1,15 @@
 package com.example.lologin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayout
+
 
 class AlarmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarms)
+        //Navigation bar value
+        val navigationBar = findViewById<TabLayout>(R.id.navigation_bar)
     }
 }
