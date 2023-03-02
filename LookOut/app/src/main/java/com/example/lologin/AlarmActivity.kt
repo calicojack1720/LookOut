302115284d135f5de4ff5ae43bdd6bf2722d579d
Pine-1 <droskeadam@gmail.com>
2023-03-02 14:27:46 -0500

    Added new Listener for AddAlarm Button in AlarmActivity.kt Working on creating popup Window for Add Alarm Button

--- LookOut/app/src/main/java/com/example/lologin/AlarmActivity.kt
+++ LookOut/app/src/main/java/com/example/lologin/AlarmActivity.kt
@@ -1,7 +1,12 @@
 package com.example.lologin
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.PopupWindow
 import androidx.appcompat.app.AppCompatActivity
 import com.example.lologin.ui.login.LoginActivity
 import com.google.android.material.tabs.TabLayout
@@ -31,5 +36,15 @@ class AlarmActivity : AppCompatActivity() {
                 // Handle tab reselection
             }
         })
+        //New Code -- ABD
+        //Goal - Create a listener that opens a window to add new alarms
+        val addAlarmButton = findViewById<Button>(R.id.addalarm)
+        addAlarmButton.setOnClickListener {
+            val popupView = layoutInflater.inflate(R.layout.popup_layout, null)
+            val popupWindow = PopupWindow(popupView, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
+            popupWindow.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            popupWindow.animationStyle = R.style.PopupAnimation
+            popupWindow.showAsDropDown(addAlarmButton)
+        }
     }
 }
