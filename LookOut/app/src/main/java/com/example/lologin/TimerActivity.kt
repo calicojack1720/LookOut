1986de7319bc0350bc6acc48c626dcdd6def362d
Michael Astfalk <mtastfalkjr@verizon.net>
2023-03-22 22:22:50 -0400

    Added vals for Timer Activity layout, TimerHours, TimerMinutes, TimerSeconds, and StartTimer in TimerActivity.kt

--- LookOut/app/src/main/java/com/example/lologin/TimerActivity.kt
+++ LookOut/app/src/main/java/com/example/lologin/TimerActivity.kt
@@ -2,10 +2,21 @@ package com.example.lologin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Button
 
 class TimerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timers)
+
+        //Create val for timer_item.xml
+        val timerView = layoutInflater.inflate(R.layout.activity_timers, null)
+
+        val inputTimerHours = timerView.findViewById<EditText>(R.id.TimerHours)     //hours input
+        val inputTimerMinutes = timerView.findViewById<EditText>(R.id.TimerMinutes) //minutes input
+        val inputTimerSeconds = timerView.findViewById<EditText>(R.id.TimerSeconds) //seconds input
+
+        val startTimerButton = timerView.findViewById<Button>(R.id.StartTimer)  //start timer button
     }
 }
