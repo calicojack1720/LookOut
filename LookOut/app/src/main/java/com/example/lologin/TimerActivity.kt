f6567686739c1739c6ec338b352e51b99e5c143d
Michael Astfalk <mtastfalkjr@verizon.net>
2023-04-10 19:57:30 -0400

    Fixed bug where countdown continued after reset.

--- LookOut/app/src/main/java/com/example/lologin/TimerActivity.kt
+++ LookOut/app/src/main/java/com/example/lologin/TimerActivity.kt
@@ -1,8 +1,9 @@
 /* TimerActivity.kt
    Initiates the timers page and handles setting, starting, stopping, and creating and using Tiemrs.
    Created by Michael Astfalk
+   Edited by Matthew Alexander
    Created: 3/17/2023
-   Updated: 4/8/2023
+   Updated: 4/10/2023
  */
 
 
@@ -168,14 +169,19 @@ class TimerActivity : AppCompatActivity() {
 
                 override fun onTick(millisUntilFinished: Long) {
                     //check continueCountDown, finish if false
-                    if(!continueCountDown)
+                    if(!continueCountDown) {
                         cancel()
-
-                    //update countHours, countMinutes, and countSeconds
-                    getTimeLeft()
-                    inputTimerHours.setText("$countHours")
-                    inputTimerMinutes.setText("$countMinutes")
-                    inputTimerSeconds.setText("$countSeconds")
+                        inputTimerHours.setText("")
+                        inputTimerMinutes.setText("")
+                        inputTimerMinutes.setText("")
+                    }
+                    else {
+                        //update countHours, countMinutes, and countSeconds
+                        getTimeLeft()
+                        inputTimerHours.setText("$countHours")
+                        inputTimerMinutes.setText("$countMinutes")
+                        inputTimerSeconds.setText("$countSeconds")
+                    }
                 }
 
                 override fun onFinish() {
@@ -208,6 +214,9 @@ class TimerActivity : AppCompatActivity() {
             resetTimer.setOnClickListener {
                 //cancel timer
                 timerItem?.let{scheduler.cancel(it)}
+
+                //cancel the countdown
+                continueCountDown = false
 
                 //reset button colors
                 resetTimer.setBackgroundColor(Color.DKGRAY)
