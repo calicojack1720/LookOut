9098c4ca86b3dbfaa37364122d5b852bea163a2e
Michael Astfalk <mtastfalkjr@verizon.net>
2023-04-18 00:36:10 -0400

    Created TimerConvertToSecTest.kt

---
+++ LookOut/app/src/androidTest/java/com/example/lologin/TimerConvertToSecTest.kt
@@ -0,0 +1,13 @@
+/* TimerConvertToSecTest.kt
+   Tests the ConvertToSec function in TimerActivity.kt
+   Created by Michael Astfalk
+   Created: 4/18/2023
+   Edited: 4/18/2023
+ */
+
+package com.example.lologin
+
+
+
+class TimerConvertToSecTest {
+}
